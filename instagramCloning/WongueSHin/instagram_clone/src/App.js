9aa8aaed9c8619172642836582f4575159ac8372
WongueShin <32152412@dankook.ac.kr>
2021-08-03 16:31:54 +0900

    Rewrite comp Class형 comp -> Func형 comp로...

--- instagramCloning/WongueSHin/instagram_clone/src/App.js
+++ instagramCloning/WongueSHin/instagram_clone/src/App.js
@@ -1,57 +1,54 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class App extends Component {
-  state = {
-    boards: [
+const App = () => {
+  const[board, setBoard] = useState(
+    [
       {
-        brdno:1,
-        brdwriter: 'Lee SunSin',
+        brdno: 1,
+        brdwriter: 'Lee Sunsin',
         brdtitle: 'If you intend to live then you die',
         brddate: new Date()
       },
       {
         brdno: 2,
-        brdwriter: 'So SiNo',
-        brdtitle: 'Founder for two countries',
+        brdwriter: 'Lee sin',
+        brdtitle: 'Where to go',
         brddate: new Date()
       }
     ]
-  }
+  );
 
-  render() {
-    const { boards } = this.state;
-   return (
-      <div>
-          <table border="1">
-            <tbody>
-              <tr align="center">
-                <td width="50">No.</td>
-                <td width="300">Title</td>
-                <td width="100">Date</td>
-              </tr>
-              {
-                boards.map(
-                  row => (<BoardItem key={row.brdno} row={row} />)
-                )
-              }
-            </tbody>
-          </table>
-      </div>
-    );
-  }
+  return(
+    <div>
+      <table border ="1">
+        <tbody>
+          <tr align="center">
+            <td width = "50">No.</td>
+            <td width = "300">Title</td>
+            <td width = "100">Name</td>
+            <td width = "100">Date</td>
+          </tr>
+          {
+            board.map(post => 
+              (
+              <BoardItem key = {post.brdno} post={post}/>
+              ))
+          }
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
-class BoardItem extends React.Component {
-  render() {
-    return(
-      <tr>
-        <td>{this.props.row.brdno}</td>
-        <td>{this.props.row.brdtitle}</td>
-        <td style={{textAlign :'center'}}>{this.props.row.brdwriter}</td>
-        <td>{this.props.row.brddate.toLocaleDateString('ko-KR')}</td>
-      </tr>
-    );
-  }
+function BoardItem(props) {
+  return(
+    <tr>
+      <td>{props.post.brdno}</td>
+      <td>{props.post.brdtitle}</td>
+      <td>{props.post.brdwriter}</td>
+      <td>{props.post.brddate.toLocaleDateString('ko-KR')}</td>
+    </tr>
+  );
 }
 
 export default App;
