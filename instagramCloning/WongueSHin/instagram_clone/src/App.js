d79a8e1be6513e497904e7bd6d39f20314d4d7dc
WongueShin <32152412@dankook.ac.kr>
2021-08-10 16:06:58 +0900

    Add literature and react hook example

--- instagramCloning/WongueSHin/instagram_clone/src/App.js
+++ instagramCloning/WongueSHin/instagram_clone/src/App.js
@@ -1,85 +1,97 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
+import propTypes, { array, number } from 'prop-types';
 
 const App = () => {
-  let[maxNo, setmaxNo] = useState(2);
-  let[board, setBoard] = useState(
-    [
-      {
-        brdno: 1,
-        brdwriter: 'Lee Sunsin',
-        brdtitle: 'If you intend to live then you die',
-        brddate: new Date()
-      },
-      {
-        brdno: 2,
-        brdwriter: 'Lee sin',
-        brdtitle: 'Where to go',
-        brddate: new Date()
-      }
-    ]
-  );
+  let movieList = [
+    {
+      movieName: "2001: A Space Odyssey",
+      movieposter:"https://media.timeout.com/images/105455969/1024/768/image.jpg",
+      releasesDates: "April 2, 1968"
+    },
+    {
+      movieName: "The Godfather",
+      movieposter:"https://media.timeout.com/images/105455970/1024/768/image.jpg",
+      releasesDates: "May 25, 1977"
+    },
+    {
+      movieName: "Citizen Kane",
+      movieposter:"https://media.timeout.com/images/105455971/1024/768/image.jpg",
+      releasesDates: "May 1 1941"
+    },
+    {
+      movieName: "Seven Samurai",
+      movieposter:"https://media.timeout.com/images/101714537/1024/768/image.jpg",
+      releasesDates: "April 26, 1954"
+    },
+    {
+      movieName: "Star Wars",
+      movieposter:"https://media.timeout.com/images/105456000/1024/768/image.jpg",
+      releasesDates: ""
+    },
+    {
+      movieName: "PULP Fiction",
+      movieposter:"https://media.timeout.com/images/105456005/1024/768/image.jpg",
+      releasesDates: "September 10, 1994"
+    },
+    {
+      movieName: "The Truman Show",
+      movieposter:"https://media.timeout.com/images/101630177/1024/768/image.jpg",
+      releasesDates: "October 24 1998"
+    },
+  ]
+  let [index, setIndex] = useState(0);
+  let [isLoading, setIsLoading] = useState(true);
 
-  const handleSaveData = (data) => {
-    console.log('codelogic04');
-    setmaxNo(maxNo++);
-    setBoard(board.concat(
-      {brdno:maxNo, brddate: new Date(), ...data}
-    ));
-    console.log(board);
+  useEffect( () => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 6000);
+  }, []);
+
+  const add = () => {
+    if (index === movieList.length - 1) {
+      setIndex(0);
+    }
+    else {
+      setIndex(index+1)
+    }
+    console.log(index);
   }
+  const minus = () =>{
+    if (index === 0) {
+      setIndex(movieList.length - 1)
+    }
+    else {
+      setIndex(index-1)
+    }
+    console.log(index);
+  }
+    return(
+      <div>
+        <p>{isLoading ? "Loading...": "We are ready!"}</p>
+        <MoviePost idx={index} listOfMoive={movieList}/>
+        <button onClick={minus}>before</button>
+        <button onClick={add}>next</button>
+      </div>
+    );
+}
+
+function MoviePost ({idx, listOfMoive}) {
+  let index = idx;
+  let list = listOfMoive
 
   return(
-    <div>
-      <BoardForm onSaveData = {handleSaveData} state= {{}}/>
-
-      <table border ="1">
-        <tbody>
-          <tr align="center">
-            <td width = "50">No.</td>
-            <td width = "300">Title</td>
-            <td width = "100">Name</td>
-            <td width = "100">Date</td>
-          </tr>
-          {
-            board.map(post => 
-              (
-              <BoardItem key = {post.brdno} post={post}/>
-              ))
-          }
-        </tbody>
-      </table>
-    </div>
+  <div>
+    <h4>Movie Title: {list[index].movieName}</h4>
+    <h5>released in: {list[index].releasesDates}</h5>
+    <img src= {list[index].movieposter} height= "350px"></img>
+  </div>
   );
 }
 
-const BoardForm = (props) => {
-  let state = {}
-
-  const handleChange = (e) => {
-    state[e.target.name]= e.target.value
-  }
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    props.onSaveData(state);
-  }  
-  return(
-    <form onSubmit={handleSubmit}>
-      <input placeholder="title" name="brdtitle" onChange={handleChange}/>
-      <input placeholder="name" name="brdwriter" onChange={handleChange}/>
-      <button type="submit">Save</button>
-    </form>
-  );
-}
-
-function BoardItem(props) {
-  return(
-    <tr>
-      <td>{props.post.brdno}</td>
-      <td>{props.post.brdtitle}</td>
-      <td>{props.post.brdwriter}</td>
-      <td>{props.post.brddate.toLocaleDateString('ko-KR')}</td>
-    </tr>
-  );
+MoviePost.propTypes = {
+  idx: number,
+  listOfMoive: array
 }
 
 export default App;
