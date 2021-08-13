9bfe0523351d6abdf18995f18ed95fa3eef1a87b
WongueShin <32152412@dankook.ac.kr>
2021-08-13 21:33:39 +0900

    import new module "axios"

--- instagramCloning/WongueSHin/instagram_clone/src/App.js
+++ instagramCloning/WongueSHin/instagram_clone/src/App.js
@@ -1,97 +1,38 @@
-import React, {useEffect, useState} from 'react';
-import propTypes, { array, number } from 'prop-types';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Proptypes from "prop-types";
+import MoviePost from "./Compnent/Movie";
+const API_URL = "https://yts-proxy.nomadcoders1.now.sh/list_movies.json";
 
 const App = () => {
-  let movieList = [
-    {
-      movieName: "2001: A Space Odyssey",
-      movieposter:"https://media.timeout.com/images/105455969/1024/768/image.jpg",
-      releasesDates: "April 2, 1968"
-    },
-    {
-      movieName: "The Godfather",
-      movieposter:"https://media.timeout.com/images/105455970/1024/768/image.jpg",
-      releasesDates: "May 25, 1977"
-    },
-    {
-      movieName: "Citizen Kane",
-      movieposter:"https://media.timeout.com/images/105455971/1024/768/image.jpg",
-      releasesDates: "May 1 1941"
-    },
-    {
-      movieName: "Seven Samurai",
-      movieposter:"https://media.timeout.com/images/101714537/1024/768/image.jpg",
-      releasesDates: "April 26, 1954"
-    },
-    {
-      movieName: "Star Wars",
-      movieposter:"https://media.timeout.com/images/105456000/1024/768/image.jpg",
-      releasesDates: ""
-    },
-    {
-      movieName: "PULP Fiction",
-      movieposter:"https://media.timeout.com/images/105456005/1024/768/image.jpg",
-      releasesDates: "September 10, 1994"
-    },
-    {
-      movieName: "The Truman Show",
-      movieposter:"https://media.timeout.com/images/101630177/1024/768/image.jpg",
-      releasesDates: "October 24 1998"
-    },
-  ]
-  let [index, setIndex] = useState(0);
   let [isLoading, setIsLoading] = useState(true);
+  let [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    setMovies((await axios.get(API_URL)).data.data.movies);
+    setIsLoading(false);
+  }
 
   useEffect( () => {
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 6000);
-  }, []);
+    getMovies();
+  },[]);
 
-  const add = () => {
-    if (index === movieList.length - 1) {
-      setIndex(0);
-    }
-    else {
-      setIndex(index+1)
-    }
-    console.log(index);
-  }
-  const minus = () =>{
-    if (index === 0) {
-      setIndex(movieList.length - 1)
-    }
-    else {
-      setIndex(index-1)
-    }
-    console.log(index);
-  }
-    return(
-      <div>
-        <p>{isLoading ? "Loading...": "We are ready!"}</p>
-        <MoviePost idx={index} listOfMoive={movieList}/>
-        <button onClick={minus}>before</button>
-        <button onClick={add}>next</button>
-      </div>
-    );
-}
-
-function MoviePost ({idx, listOfMoive}) {
-  let index = idx;
-  let list = listOfMoive
+  console.log(movies, isLoading);
 
   return(
-  <div>
-    <h4>Movie Title: {list[index].movieName}</h4>
-    <h5>released in: {list[index].releasesDates}</h5>
-    <img src= {list[index].movieposter} height= "350px"></img>
-  </div>
+    <section class = "container">
+      {isLoading ? 
+      <div>
+      <span class= "loader">now Loading...</span>
+      </div>
+      :
+      movies.map(props => (
+        <MoviePost key = {movies.indexOf(props)} data ={props}/>
+      ))
+      }
+    </section>
   );
 }
 
-MoviePost.propTypes = {
-  idx: number,
-  listOfMoive: array
-}
 
-export default App;
+ export default App;
