71389f973e4a1aaf7cbb03bf9c5bf0ad972b6cc5
sApOOn <won7963kr@gmail.com>
2021-08-16 23:09:24 +0900

    instagram main page clonning

--- instagramCloning/WongueSHin/instagram_clone/src/App.js
+++ instagramCloning/WongueSHin/instagram_clone/src/App.js
@@ -1,37 +1,21 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Proptypes from "prop-types";
-import MoviePost from "./Compnent/Movie";
-const API_URL = "https://yts-proxy.nomadcoders1.now.sh/list_movies.json";
+import {Route} from 'react-router-dom'
+import "./App.css";
+
+import Home from "./Pages/Home"
+
+const LOGIN_CHECK = true;
 
 const App = () => {
-  let [isLoading, setIsLoading] = useState(true);
-  let [movies, setMovies] = useState([]);
-
-  const getMovies = async () => {
-    setMovies((await axios.get(API_URL)).data.data.movies);
-    setIsLoading(false);
+    if (LOGIN_CHECK) {
+      return(   
+        <div id="App">
+          <Home/>
+        </div>
+      );
   }
-
-  useEffect( () => {
-    getMovies();
-  },[]);
-
-  console.log(movies, isLoading);
-
-  return(
-    <section class = "container">
-      {isLoading ? 
-      <div>
-      <span class= "loader">now Loading...</span>
-      </div>
-      :
-      movies.map(props => (
-        <MoviePost key = {movies.indexOf(props)} data ={props}/>
-      ))
-      }
-    </section>
-  );
 }
 
 
---
+++ instagramCloning/WongueSHin/instagram_clone/src/Pages/Home.js
@@ -0,0 +1,59 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import {Route} from "react-router-dom";
+import NavigationBar from './Component/NavigationBar';
+import MainFeed from './Component/MainFeed';
+import Account from './Component/Account';
+import Loading from "./Component/Loading";
+import Post from "./Component/Post";
+
+import './Home.css';
+import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
+
+const Home = () => {
+    /*
+    let [isLoading, setIsLoading] = useState(true);
+
+    if (isLoading){
+        setTimeout(() => {
+            console.log(isLoading);
+            setIsLoading(false);
+            console.log(isLoading);
+        }, 2000);
+    }
+    */
+    let [isLoading, setIsLoading] = useState(false);
+
+    let[navigateProps, setNavigateProps] = useState(null);
+
+    if (navigateProps == null) {
+        setNavigateProps(
+            {
+                user_small_profile : 'Anonymous',
+            }
+        )
+    }
+
+    return(
+        isLoading?
+        <div id="Home"> 
+        <Loading/>
+        </div> :
+        <BrowserRouter>
+            <div id = "main">
+            <NavigationBar props= {navigateProps}/>
+            <MainFeed/>
+            <Account/>
+            <Route exact path="/post" component={Post}/>
+            </div>
+        </BrowserRouter>
+    );
+   
+}
+
+    
+function loadPages() {
+    
+}
+
+export default Home
