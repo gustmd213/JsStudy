90f649f948dbf23ce393308d5c0fd872d5c36f1b
WongueShin <32152412@dankook.ac.kr>
2021-08-17 10:09:16 +0900

    navigation bar css changed vmax -> em

--- instagramCloning/WongueSHin/instagram_clone/src/Pages/Home.js
+++ instagramCloning/WongueSHin/instagram_clone/src/Pages/Home.js
@@ -11,9 +11,9 @@ import './Home.css';
 import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
 
 const Home = () => {
-    /*
+    
     let [isLoading, setIsLoading] = useState(true);
-
+    
     if (isLoading){
         setTimeout(() => {
             console.log(isLoading);
@@ -21,8 +21,7 @@ const Home = () => {
             console.log(isLoading);
         }, 2000);
     }
-    */
-    let [isLoading, setIsLoading] = useState(false);
+    
 
     let[navigateProps, setNavigateProps] = useState(null);
 
