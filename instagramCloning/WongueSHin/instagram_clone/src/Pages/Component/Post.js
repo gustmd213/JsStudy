28983b6f88481dd1a7a924338e71bee66045c53f
WongueShin <32152412@dankook.ac.kr>
2021-08-19 16:42:51 +0900

    refac exit post

--- instagramCloning/WongueSHin/instagram_clone/src/Pages/Component/Post.js
+++ instagramCloning/WongueSHin/instagram_clone/src/Pages/Component/Post.js
@@ -3,26 +3,54 @@ import ReactRouterDom, { Link } from 'react-router-dom'
 import './Post.css';
 
 
-const Post = () => {
+const Post = (props) => {
+    const history = props.history;
     const [postImage, setPostImage] = useState('test');
     const [postText, setPostText] = useState(null);
+    const [shouldPopup, setShouldPopUp] = useState(false);
     
 
     return(
-        <Wait poststate = {postImage} postsetter = {setPostImage}/>
+        <Wait history = {history} poststate = {postImage} postsetter = {setPostImage} shouldPopup={shouldPopup} setShouldPopUp={setShouldPopUp}/>
     )
 }
 
-function Wait ({poststate, postsetter}) {
+function Wait ({history, poststate, postsetter, shouldPopup, setShouldPopUp}) {
     let postImage = poststate;
     let setPostImage = postsetter;
     const imageInput = useRef();
-    return(
-        <div id = 'container'>
+    console.log(shouldPopup);
+    if (shouldPopup === true){
+        return(
+        <div>
+        <div id = 'container' className='exit' >
             <div id="box">
                 <div id ="upline">
                     <h4 id='title'>새 게시물</h4>
-                    <ExitPost postImage = {postImage} Exitele = {<span id='xmark'>X</span>}/>
+                    <span id='xmark' className='exit'>X</span>
+                </div>
+                <div id = 'postbox'>
+                <img id = 'picture'src='/Post/picturenmovie.png'/>
+                <span id = 'text'>
+                    사진과 동영상을 여기에 끌어다 놓으세요.
+                </span>
+                <form id = 'fileform' method='post' encType='multipart/form-data' onSubmit={(event)=>{event.preventDefault()}}>
+                    <button id = 'selectbutton' onClick={()=> {imageInput.current.click()}}>컴퓨터에서 선택</button>
+                    <input type='file' id='chooseFile' name='chooseFile' accept='image/*' onChange={e => {loadFile(e)}} ref={imageInput}/>
+                </form>
+                </div>
+            </div>
+        </div>
+        <AskOneMore stateSetter={setShouldPopUp}/>
+        </div>
+        );
+    }
+    return(
+        <div id = 'container' className='exit' onClick={(e)=>{ExitPost(e, postImage, history, setShouldPopUp)}}>
+            <div id="box">
+                <div id ="upline">
+                    <h4 id='title'>새 게시물</h4>
+                    <span id='xmark' className='exit'>X</span>
                 </div>
                 <div id = 'postbox'>
                 <img id = 'picture'src='/Post/picturenmovie.png'/>
@@ -43,31 +71,14 @@ function loadFile(e){
     console.log(e.target.files[0]);
 }
 
-function ExitPost (props) {
-    const postImage = props.postImage;
-    const Exitele = props.Exitele;
-    const [shouldPopUp, setShouldPopUp] = useState(false);
-    if (postImage === null) {
-        return(
-        <Link to ='/'>
-            {Exitele}
-        </Link>
-        );
+function ExitPost (e, postImage, history, popupSetter) {
+    console.log(popupSetter);
+    if(e.target.className === 'exit' && postImage === null){
+        return history.push('/');
     }
-    if (shouldPopUp){
     return(
-        <div>
-        <span>{Exitele}</span>
-        <AskOneMore stateSetter = {setShouldPopUp}/>
-        </div>
+    popupSetter(true)
     );
-    }
-    return(
-        <span onClick={() => {setShouldPopUp(true)}}>
-        {Exitele}
-        </span>
-    )
-    return null
 }
 
 const AskOneMore = ({stateSetter}) => {
